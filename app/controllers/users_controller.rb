b6d96e81581e97366f2eeecfe284bcc9a36a3582
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-16 18:57:02 +0000

    Gives errors when registering fails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/register'
+      render :new
     end
   end
 
