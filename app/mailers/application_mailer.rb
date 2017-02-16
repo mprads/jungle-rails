861341810e645ba5164e847bb8d5497d9ca41b05
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-16 23:24:02 +0000

    Created basic layout for email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "noreply@jungle.com"
   layout 'mailer'
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
 end
