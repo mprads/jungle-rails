b8fa7b91cd4c5fd615e8d6afc0aee306e21c4b9b
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-16 18:48:35 +0000

    Fixed registration page layout

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -22,7 +22,7 @@ class Admin::CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(
-      :name,
+      :name
     )
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
@@ -17,6 +17,12 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
