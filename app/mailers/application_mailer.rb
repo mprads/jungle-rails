e2b34bd38dea0ffe5fb81e6c3d7187cee68adbc3
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-17 00:23:29 +0000

    Created preview to see email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "noreply@jungle.com"
   layout 'mailer'
-
-  def welcome_email(user)
-    @order = order
-    mail(to: @order.email, subject: 'Order number #{@order.id}')
-  end
-
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  default from: "noreply@jungle.com"
+ def welcome_email(order)
+    @order = order
+    mail(to: @order.email, subject: "Order number #{@order.id}")
+  end
 end
