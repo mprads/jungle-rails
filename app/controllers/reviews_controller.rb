ed167b007ba32aa83d45ec096c6f264803edb9bf
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-17 22:23:05 +0000

    Created and moved reviews into partial

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+  def create
+    @product = Product.find params[:product_id]
+    @review = @product.review.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to "/products/#{@review.product_id}"
+    else
+      redirect_to "/"
+    end
+  end
+  private
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
+  end
 end
