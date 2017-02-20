1390d9e2446d28c3e31ef6da181d47c5417d57cd
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-20 22:24:19 +0000

    Added length validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password_confirmation, presence: true
+  validate :password length: { minimum: 1 }
+  validates :password_confirmation, presence: true, length: { minimum: 5 }
 end
