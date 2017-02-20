6e8cc7712671841c2c85eb8c9ceeeb70da5fc3b5
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-20 20:09:34 +0000

    Created 5 test that all pass and check validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,44 +4,55 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
       let(:category) { Category.create(name: 'test')}
 
-      it 'Should require a name' do
+      it 'should require a name' do
         @product = category.products.create({
           name: nil,
           description: 'Test testing',
           price: 10.99,
           quantity: 5
           })
-        expect(@product.id).to be_nil
+        expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
-      it 'Should require a description' do
-        @product = category.products.create({
-          name: 'Tester',
-          description: nil,
-          price: 10.99,
-          quantity: 5
-          })
-        expect(@product.id).to be_nil
-    end
 
-    it 'Should require a price' do
+    it 'should require a price' do
         @product = category.products.create({
           name: 'Tester',
           description: 'Test testing',
           price: nil,
           quantity: 5
           })
-        expect(@product.id).to be_nil
+        expect(@product.errors.full_messages).to include("Price cents is not a number", "Price is not a number", "Price can't be blank")
     end
 
-    it 'Should require a quantity' do
+    it 'should require a quantity' do
         @product = category.products.create({
           name: 'Tester',
           description: 'Test testing',
           price: 10.99,
           quantity: nil
           })
-        expect(@product.id).to be_nil
+        expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+     it 'should require a category' do
+        @product = Product.create({
+          name: 'Tester',
+          description: 'Test testing',
+          price: 10.99,
+          quantity: 5
+          })
+        expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it 'should save' do
+        @product = category.products.create({
+          name: 'Tester',
+          description: 'Test testing',
+          price: 10.99,
+          quantity: 10
+          })
+        expect(@product.id).to be_truthy
     end
 
   end
