33de49e3868cc9b7d3f433985a58ac2cfcfa6eec
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-20 19:44:01 +0000

    First test is functional

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,46 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+      let(:category) { Category.create(name: 'test')}
+
+      it 'Should require a description' do
+        @product = category.products.create({
+          name: 'Tester',
+          description: nil,
+          price: 10.99,
+          quantity: 5
+          })
+        expect(@product.id).to_be nil
+    end
+
+    it 'Should require a price' do
+        @product = category.products.create({
+          name: 'Tester',
+          description: 'Test testing',
+          price: nil,
+          quantity: 5
+          })
+        expect(@product.id).to_be nil
+    end
+
+    it 'Should require a name' do
+        @product = category.products.create({
+          name: nil,
+          description: 'Test testing',
+          price: 10.99,
+          quantity: 5
+          })
+        expect(@product.id).to_be nil
+    end
+
+    it 'Should require a name' do
+        @product = category.products.create({
+          name: nil,
+          description: 'Test testing',
+          price: 10.99,
+          quantity: 5
+          })
+        expect(@product.id).to_be nil
+    end
   end
 end
