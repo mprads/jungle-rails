484e0228c08a5e1a09f67a20127bea071f797aaf
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-21 21:41:44 +0000

    Test verifies cart had one item added

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to a product page", type: :feature, js: true do
+
+  before :each do
+      @category = Category.create! name: 'Apparel'
+
+      10.times do |n|
+        @category.products.create!(
+          name:  Faker::Hipster.sentence(3),
+          description: Faker::Hipster.paragraph(4),
+          image: open_asset('apparel1.jpg'),
+          quantity: 10,
+          price: 64.99
+        )
+      end
+    end
+
+    scenario "Add item to cart" do
+      visit root_path
+
+      save_screenshot 'empty-cart.png'
+
+      first('article.product').find_link('Add').click
+      save_screenshot 'item-in-cart.png'
+      expect(page).to have_content 'My Cart (1)'
+    end
+
 end
